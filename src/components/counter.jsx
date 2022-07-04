46f2d02857563ca603009328f1d7b56ff1220d78
den1aka1aki <lavrov.den@gmail.com>
2022-07-04 06:24:01 +0200

    lesson 04/07/22

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Counter = () =>{
-    let count = 0;
+
+    const [count, setCount] = useState(0);
     const formatCount= () => {
         return count === 0 ? 'empty' : count;
     };
@@ -12,14 +13,26 @@ const Counter = () =>{
     };
 
     const handleIncrement = () =>{
-        count = count +1;
+        setCount((prevState) =>prevState+1);
+    };
+    const handleDecrement = () =>{
+        setCount((prevState) => prevState-1);
     };
 
     return (
         <>
 
         <span className={getBageClasses()}> {formatCount()} </span>
-        <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
+        <button
+            className='btn btn-primary btn-sm m-2'
+            onClick={handleIncrement}>
+            +
+        </button>
+            <button
+                className='btn btn-primary btn-sm m-2'
+                onClick={handleDecrement}>
+                -
+            </button>
     </>
     );
 };
