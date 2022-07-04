c378adae810454aea2b9d3d6f5618f3b04716e11
den1aka1aki <lavrov.den@gmail.com>
2022-07-04 06:07:40 +0200

    lesson 04/07/22

---
+++ src/components/counter.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Counter = () =>{
+    let count = 0;
+    const formatCount= () => {
+        return count === 0 ? 'empty' : count;
+    };
+    const getBageClasses = () =>{
+        let classes = 'badge m-2 ';
+        classes += count===0?'bg-warning':'bg-primary'
+        return classes;
+    };
+
+    const handleIncrement = () =>{
+        count = count +1;
+    };
+
+    return (
+        <>
+
+        <span className={getBageClasses()}> {formatCount()} </span>
+        <button className='btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
+    </>
+    );
+};
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import reactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.css"
+import Counter from "./components/counter";
 
-const element = <h1> Hello Wolrd </h1>
 
-reactDom.render(element, document.getElementById('root'));
+
+const App = () =>{
+    return <Counter/>;
+}
+reactDom.render(<App/>, document.getElementById('root'));
