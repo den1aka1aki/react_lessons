2327f5a0d6e3b0c35c4b8165c02bc8f84ca73216
den1aka1aki <lavrov.den@gmail.com>
2022-07-14 09:52:31 +0200

    new lessons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,39 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const Counter = () =>{
+const Counter = (props) => {
+  const {value}=props
 
-    const [count, setCount] = useState(0);
-    const formatCount= () => {
-        return count === 0 ? 'empty' : count;
-    };
-    const getBageClasses = () =>{
-        let classes = 'badge m-2 ';
-        classes += count===0?'bg-warning':'bg-primary'
-        return classes;
-    };
+  const formatValue = () => {
+    return value === 0 ? "empty" : value;
+  };
+  const getBageClasses = () => {
+    let classes = "badge m-2 ";
+    classes += value === 0 ? "bg-warning" : "bg-primary";
+    return classes;
+  };
 
-    const handleIncrement = () =>{
-        setCount((prevState) =>prevState+1);
-    };
-    const handleDecrement = () =>{
-        setCount((prevState) => prevState-1);
-    };
+  const handleIncrement = () => {
+   // setValue((prevState) => prevState + 1);
+  };
+  const handleDecrement = () => {
+   // setValue((prevState) => prevState - 1);
+  };
 
-    return (
-        <>
-
-        <span className={getBageClasses()}> {formatCount()} </span>
-        <button
-            className='btn btn-primary btn-sm m-2'
-            onClick={handleIncrement}>
-            +
-        </button>
-            <button
-                className='btn btn-primary btn-sm m-2'
-                onClick={handleDecrement}>
-                -
-            </button>
-    </>
-    );
+  return (
+    <div>
+      <span>{props.name}</span>
+      <span className={getBageClasses()}> {formatValue()} </span>
+      <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+        +
+      </button>
+      <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
+        -
+      </button>
+      <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => props.onDelete(props.id)}>
+        Delete
+      </button>
+    </div>
+  );
 };
 export default Counter;
